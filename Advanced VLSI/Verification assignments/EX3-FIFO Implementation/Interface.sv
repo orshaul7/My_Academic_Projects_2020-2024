bdece4cac3776566ae398b488a38d055dac8f205
orshaul7 <orshaulcr7@gmail.com>
2024-10-19 21:23:42 +0300

    Update Interface.sv

--- Advanced VLSI/Verification assignments/EX3-FIFO Implementation/Interface.sv
+++ Advanced VLSI/Verification assignments/EX3-FIFO Implementation/Interface.sv
@@ -1,1 +1,15 @@
 
+interface fifo_if (input logic clk, input logic rstn);
+	// 2. Interface Definition
+
+	logic wr_req;
+	logic rd_req;
+	logic [WIDTH-1:0] rd_data; 
+	logic [WIDTH-1:0] wr_data;
+	logic full_o;
+	logic empty_o; 	
+    
+
+endinterface : fifo_if
+
+
